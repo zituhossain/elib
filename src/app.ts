bb80edb40ec9d39225f2f48bfc4f2661477874bc
zituhossain5 <zituhossain@gmail.com>
2024-06-03 16:13:26 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 // Routes
 app.get("/", (req, res, next) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "welcome to elib" });
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
